6a302ac0c8d289ddbba8f90cffc7c38f781ebca7
shahriyorteamit <110972056+shahriyorteamit@users.noreply.github.com>
2022-08-10 14:01:41 +0500

    wef
    wefew

---
+++ Новая папка/script.js
@@ -0,0 +1,40 @@
+// alert('hello');                             1. qo`shimcha oyna ko`rinishida chiqadi.
+// document.write('<h2> hello</h2>  <hr>');    2. ekranga oddiy chiqarish.
+// console.log('hello');                       3.  consoleni oynaga chiqarishimiz.
+// confirm('uzbekistoning poytahti toshkent');  4. true yoki falseni qaytaradi.
+// alert('Hello'); alert('World');
+
+// var name1 = 'Shahriyor';
+
+// var num = +prompt('birinchi sonni kiriting');
+// var num2 = +prompt('ikkinchi sonni kiriting');
+
+// document.write(num + num2);
+
+
+// //Olma
+// var olmaKg = 2;
+// var olmaBirKgNarxi = 7000;
+// var olmaHisob = olmaKg * olmaBirKgNarxi;
+
+// // Nok
+// var nokKg = 5;
+// var nokBirKgNarxi = 8000;
+// var nokHisob = nokKg * nokBirKgNarxi;
+
+// // Gilos
+// var glosKg = 3;
+// var glosBirKgNarxi = 5000;
+// var glosHisob = glosKg * glosBirKgNarxi;
+
+// // hisob 
+// var umumiyHisob = olmaHisob + nokHisob + glosHisob;
+
+// document.write(umumiyHisob);
+
+var name = 'shahriyor';
+name = 'shahriyor Xudoyorov';
+
+document.write(name);
+
+
